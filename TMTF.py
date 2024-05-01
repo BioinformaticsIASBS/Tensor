6a10bd2acca45239d042ce833e2d72f5f7093fde
kaBuki44 <javadasghari13@gmail.com>
2024-05-01 13:48:34 +0330

    Minor fix

--- TMTF.py
+++ TMTF.py
@@ -9,7 +9,7 @@ def TMTF(args: argparse.Namespace) -> np.ndarray:
     if args.dataset == 'DTINET':
         if args.comp_sim_mats:
             compute_similarity_matrices.save_to_file()
-            
+
         '''
         Load drug-target matrix
         '''
@@ -30,21 +30,24 @@ def TMTF(args: argparse.Namespace) -> np.ndarray:
         data = [row for row in csvreader]
         head = data[0]
         tail = data[1:]
-        protein_interactions = np.array([list(map(int, row[1:])) for row in tail])
+        protein_interactions = np.array(
+            [list(map(int, row[1:])) for row in tail])
 
         file = open('Data/DTINet dataset/Similarity_Matrix_Proteins.csv')
         csvreader = csv.reader(file)
         data = [row for row in csvreader]
         head = data[0]
         tail = data[1:]
-        protein_sequence_sim = np.array([list(map(float, row[1:])) for row in tail])/100
+        protein_sequence_sim = np.array(
+            [list(map(float, row[1:])) for row in tail])/100
 
         file = open('Data/DTINet dataset/protein_similarity_disease.csv')
         csvreader = csv.reader(file)
         data = [row for row in csvreader]
         head = data[0]
         tail = data[1:]
-        protein_disease_sim = np.array([list(map(float, row[1:])) for row in tail])
+        protein_disease_sim = np.array(
+            [list(map(float, row[1:])) for row in tail])
 
         '''
         Load drug similaritiy matrices
@@ -54,7 +57,8 @@ def TMTF(args: argparse.Namespace) -> np.ndarray:
         data = [row for row in csvreader]
         head = data[0]
         tail = data[1:]
-        drug_structure_sim = np.array([list(map(float, row[1:])) for row in tail])
+        drug_structure_sim = np.array(
+            [list(map(float, row[1:])) for row in tail])
 
         file = open('Data/DTINet dataset/mat_drug_drug.csv')
         csvreader = csv.reader(file)
@@ -68,7 +72,8 @@ def TMTF(args: argparse.Namespace) -> np.ndarray:
         data = [row for row in csvreader]
         head = data[0]
         tail = data[1:]
-        drug_disease_sim = np.array([list(map(float, row[1:])) for row in tail])
+        drug_disease_sim = np.array(
+            [list(map(float, row[1:])) for row in tail])
 
         file = open('Data/DTINet dataset/drug_similarity_se.csv')
         csvreader = csv.reader(file)
@@ -81,21 +86,24 @@ def TMTF(args: argparse.Namespace) -> np.ndarray:
         Create tensors
         '''
         script_T = np.concatenate((np.expand_dims(protein_sequence_sim, axis=-1),
-                                      np.expand_dims(protein_interactions, axis=-1),
-                                      np.expand_dims(protein_disease_sim, axis=-1)), axis=-1)
+                                   np.expand_dims(
+                                       protein_interactions, axis=-1),
+                                   np.expand_dims(protein_disease_sim, axis=-1)), axis=-1)
 
         script_D = np.concatenate((np.expand_dims(drug_structure_sim, axis=-1),
-                                      np.expand_dims(drug_ineractions, axis=-1),
-                                      np.expand_dims(drug_disease_sim, axis=-1),
-                                      np.expand_dims(drug_se_sim, axis=-1)), axis=-1)
+                                   np.expand_dims(drug_ineractions, axis=-1),
+                                   np.expand_dims(drug_disease_sim, axis=-1),
+                                   np.expand_dims(drug_se_sim, axis=-1)), axis=-1)
 
         '''
         Cross-validate and predict
         '''
         pos_sample_indices = np.where(X == 1)
         neg_sample_indices = np.where(X == 0)
-        pos_sample_splits = KFold(n_splits=args.k, shuffle=True).split(pos_sample_indices[0])
-        neg_sample_splits = KFold(n_splits=args.k, shuffle=True).split(neg_sample_indices[0])
+        pos_sample_splits = KFold(
+            n_splits=args.k, shuffle=True).split(pos_sample_indices[0])
+        neg_sample_splits = KFold(
+            n_splits=args.k, shuffle=True).split(neg_sample_indices[0])
         X_hat = np.zeros(X.shape)
 
         fold_no = 1
@@ -104,11 +112,12 @@ def TMTF(args: argparse.Namespace) -> np.ndarray:
             log_file.write('Fold {}\n'.format(fold_no)) if args.log else None
             _, neg_test_split = next(neg_sample_splits)
 
-            pos_test_indices = (pos_sample_indices[0][pos_test_split], pos_sample_indices[1][pos_test_split])
+            pos_test_indices = (
+                pos_sample_indices[0][pos_test_split], pos_sample_indices[1][pos_test_split])
             test_indices = (np.concatenate((neg_sample_indices[0][neg_test_split],
                                             pos_sample_indices[0][pos_test_split]), axis=0),
-                             np.concatenate((neg_sample_indices[1][neg_test_split],
-                                             pos_sample_indices[1][pos_test_split]), axis=0))
+                            np.concatenate((neg_sample_indices[1][neg_test_split],
+                                            pos_sample_indices[1][pos_test_split]), axis=0))
             X_k = np.copy(X)
             X_k[pos_test_indices] = 0
 
@@ -122,13 +131,12 @@ def TMTF(args: argparse.Namespace) -> np.ndarray:
 
         return X_hat
 
-
-    
     else:
         '''
         Load drug-target matrix
         '''
-        file = open('Data/Gold standard dataset/' + args.dataset.lower() + '_admat_dgc.txt')
+        file = open('Data/Gold standard dataset/' +
+                    args.dataset.lower() + '_admat_dgc.txt')
         lines = [line for line in file]
         head = lines[0].split()
         tail = [line.split()[1:] for line in lines[1:]]
@@ -137,7 +145,8 @@ def TMTF(args: argparse.Namespace) -> np.ndarray:
         '''
         Load drug similarity matrix
         '''
-        file = open('Data/Gold standard dataset/' + args.dataset.lower() + '_simmat_dc.txt')
+        file = open('Data/Gold standard dataset/' +
+                    args.dataset.lower() + '_simmat_dc.txt')
         lines = [line for line in file]
         head = lines[0].split()
         tail = [line.split()[1:] for line in lines[1:]]
@@ -146,7 +155,8 @@ def TMTF(args: argparse.Namespace) -> np.ndarray:
         '''
         Load target similarity matrix
         '''
-        file = open('Data/Gold standard dataset/' + args.dataset.lower() + '_simmat_dg.txt')
+        file = open('Data/Gold standard dataset/' +
+                    args.dataset.lower() + '_simmat_dg.txt')
         lines = [line for line in file]
         head = lines[0].split()
         tail = [line.split()[1:] for line in lines[1:]]
@@ -157,8 +167,10 @@ def TMTF(args: argparse.Namespace) -> np.ndarray:
         '''
         pos_sample_indices = np.where(X == 1)
         neg_sample_indices = np.where(X == 0)
-        pos_sample_splits = KFold(n_splits=args.k, shuffle=True).split(pos_sample_indices[0])
-        neg_sample_splits = KFold(n_splits=args.k, shuffle=True).split(neg_sample_indices[0])
+        pos_sample_splits = KFold(
+            n_splits=args.k, shuffle=True).split(pos_sample_indices[0])
+        neg_sample_splits = KFold(
+            n_splits=args.k, shuffle=True).split(neg_sample_indices[0])
         X_hat = np.zeros(X.shape)
 
         fold_no = 1
@@ -167,11 +179,12 @@ def TMTF(args: argparse.Namespace) -> np.ndarray:
             log_file.write('Fold {}\n'.format(fold_no)) if args.log else None
             _, neg_test_split = next(neg_sample_splits)
 
-            pos_test_indices = (pos_sample_indices[0][pos_test_split], pos_sample_indices[1][pos_test_split])
+            pos_test_indices = (
+                pos_sample_indices[0][pos_test_split], pos_sample_indices[1][pos_test_split])
             test_indices = (np.concatenate((neg_sample_indices[0][neg_test_split],
                                             pos_sample_indices[0][pos_test_split]), axis=0),
-                             np.concatenate((neg_sample_indices[1][neg_test_split],
-                                             pos_sample_indices[1][pos_test_split]), axis=0))
+                            np.concatenate((neg_sample_indices[1][neg_test_split],
+                                            pos_sample_indices[1][pos_test_split]), axis=0))
             X_k = np.copy(X)
             X_k[pos_test_indices] = 0
 
@@ -186,23 +199,25 @@ def TMTF(args: argparse.Namespace) -> np.ndarray:
         return X_hat
 
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('dataset', type=str, choices=['DTINET', 'E', 'GPCR', 'IC', 'NR'],
                         help='Data to train/test the model on')
     parser.add_argument('--k', type=int, default=10, help='Number of folds in the \
                         k-fold cross-validation process')
-    parser.add_argument('--form_no', type=int, default=1, choices=range(1,5),
+    parser.add_argument('--form_no', type=int, default=1, choices=range(1, 5),
                         help='Formulation of the model')
-    parser.add_argument('--f_size', type=int, default=16, help='Number of latent variables')
-    parser.add_argument('--alpha', type=float, default=0.01, help='Learning rate')
-    parser.add_argument('--epsilon', type=float, default=1e-3, help='Stopping criterion')
+    parser.add_argument('--f_size', type=int, default=16,
+                        help='Number of latent variables')
+    parser.add_argument('--alpha', type=float,
+                        default=0.01, help='Learning rate')
+    parser.add_argument('--epsilon', type=float,
+                        default=1e-3, help='Stopping criterion')
     parser.add_argument('--comp_sim_mats', action=argparse.BooleanOptionalAction, default=False,
-                        help='Whether to compute similarity matrices or not (Set True at least once)')
+                        help='Whether to compute similarity matrices or not (set True at least once)')
     parser.add_argument('--log', action=argparse.BooleanOptionalAction, default=False,
                         help='Whether to log the optimization process')
     args = parser.parse_args()
-    
+
     X_hat = TMTF(args)
     np.savetxt('X_hat.txt', X_hat)
