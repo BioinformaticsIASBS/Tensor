c0a9a90adf9ce6b7408d98a7a544b2d23e864171
kaBuki44 <javadasghari13@gmail.com>
2024-05-05 16:08:37 +0330

    Minor fix

--- Utilities/compute_similarity_matrices.py
+++ Utilities/compute_similarity_matrices.py
@@ -2,22 +2,24 @@ import csv
 import numpy as np
 from scipy import spatial
 
+
 def find_similarity_matrix(input_matrix: np.ndarray) -> np.ndarray:
     input_matrix_len = len(input_matrix)
     similarity_matrix = np.zeros((input_matrix_len, input_matrix_len))
 
     for i in range(input_matrix_len):
-      for j in range(input_matrix_len):
-        i_j_rows_similarity = 1 - spatial.distance.cosine(input_matrix[i], input_matrix[j])
-        similarity_matrix[i][j] = i_j_rows_similarity
+        for j in range(input_matrix_len):
+            i_j_rows_similarity = 1 - spatial.distance.cosine(input_matrix[i], input_matrix[j])
+            similarity_matrix[i][j] = i_j_rows_similarity
 
     return similarity_matrix
 
+
 def save_to_file() -> None:
-    '''
+    """
     Compute drug similarity based on the drug-disease matrix
-    '''
-    in_file = open('Data/DTINet dataset/mat_drug_disease.csv')
+    """
+    in_file = open("Data/DTINet dataset/mat_drug_disease.csv")
     csvreader = csv.reader(in_file)
     data = [row for row in csvreader]
     head = data[0]
@@ -27,20 +29,19 @@ def save_to_file() -> None:
     drug_disease = np.array([list(map(int, row[1:])) for row in tail])
     similarity_matrix = find_similarity_matrix(drug_disease)
 
-    out_file = open('Data/DTINet dataset/drug_similarity_disease.csv', 'w', newline='')
+    out_file = open("Data/DTINet dataset/drug_similarity_disease.csv", "w", newline="")
     csvwriter = csv.writer(out_file)
-    csvwriter.writerow([''] + drugs)
+    csvwriter.writerow([""] + drugs)
     i = 0
     for row in similarity_matrix:
         csvwriter.writerow([drugs[i]] + row.tolist())
         i += 1
     out_file.close()
 
-
-    '''
+    """
     Compute drug similarity based on the drug-side effect matrix
-    '''
-    in_file = file = open('Data/DTINet dataset/mat_drug_se.csv')
+    """
+    in_file = file = open("Data/DTINet dataset/mat_drug_se.csv")
     csvreader = csv.reader(in_file)
     data = [row for row in csvreader]
     head = data[0]
@@ -50,20 +51,19 @@ def save_to_file() -> None:
     drug_se = np.array([list(map(int, row[1:])) for row in tail])
     similarity_matrix = find_similarity_matrix(drug_se)
 
-    out_file = open('Data/DTINet dataset/drug_similarity_se.csv', 'w', newline='')
+    out_file = open("Data/DTINet dataset/drug_similarity_se.csv", "w", newline="")
     csvwriter = csv.writer(out_file)
-    csvwriter.writerow([''] + drugs)
+    csvwriter.writerow([""] + drugs)
     i = 0
     for row in similarity_matrix:
         csvwriter.writerow([drugs[i]] + row.tolist())
         i += 1
     out_file.close()
 
-
-    '''
+    """
     Compute protein similarity based on the protein-disease matrix
-    '''
-    in_file = open('Data/DTINet dataset/mat_protein_disease.csv')
+    """
+    in_file = open("Data/DTINet dataset/mat_protein_disease.csv")
     csvreader = csv.reader(in_file)
     data = [row for row in csvreader]
     head = data[0]
@@ -73,12 +73,15 @@ def save_to_file() -> None:
     protein_disease = np.array([list(map(int, row[1:])) for row in tail])
     similarity_matrix = find_similarity_matrix(protein_disease)
 
-    out_file = open('Data/DTINet dataset/protein_similarity_disease.csv', 'w', newline='')
+    out_file = open(
+        "Data/DTINet dataset/protein_similarity_disease.csv",
+        "w",
+        newline="",
+    )
     csvwriter = csv.writer(out_file)
-    csvwriter.writerow([''] + proteins)
+    csvwriter.writerow([""] + proteins)
     i = 0
     for row in similarity_matrix:
         csvwriter.writerow([proteins[i]] + row.tolist())
         i += 1
     out_file.close()
-
--- Utilities/gs_optimizer.py
+++ Utilities/gs_optimizer.py
@@ -5,9 +5,17 @@ import io
 import time
 
 
-def optimize(X: np.ndarray, D: np.ndarray, T: np.ndarray,
-             f_size: int, alpha: float, epsilon: float,
-             test_indices: np.ndarray, log_file: io.TextIOWrapper) -> np.ndarray:
+def optimize(
+    X: np.ndarray,
+    D: np.ndarray,
+    T: np.ndarray,
+    f_size: int,
+    alpha: float,
+    epsilon: float,
+    test_indices: np.ndarray,
+    log_file: io.TextIOWrapper,
+) -> np.ndarray:
+
     I, M = X.shape
     f = f_size
 
@@ -19,7 +27,7 @@ def optimize(X: np.ndarray, D: np.ndarray, T: np.ndarray,
     optimizer = Adam(learning_rate=alpha, epsilon=1e-8)
     prev_loss = np.Inf
     converged = False
-    
+
     total_time = 0
     iteration = 0
     t1 = time.time()
@@ -29,38 +37,42 @@ def optimize(X: np.ndarray, D: np.ndarray, T: np.ndarray,
         loss = loss_function(U, V, X, test_indices)
         if np.math.isclose(prev_loss, loss, abs_tol=epsilon):
             converged = True
-            
+
         if log_file and (iteration % 50 == 0 or converged == True):
             t2 = time.time()
             total_time += t2 - t1
-            log_file.write('iteration: {}  loss: {:.3f}  time: {:.2f}\n'.format(iteration, loss, t2-t1))
+            log_file.write(
+                "iteration: {}  loss: {:.3f}  time: {:.2f}\n".format(iteration, loss, t2 - t1)
+            )
             t1 = time.time()
-            
+
         prev_loss = loss
         iteration += 1
-        
+
     if log_file:
-        log_file.write('{} iterations in {:.2f} minutes\n\n\n'.format(iteration, total_time/60))
- 
+        log_file.write("{} iterations in {:.2f} minutes\n\n\n".format(iteration, total_time / 60))
+
     return np.matmul(U, V)
-    
 
 
 def loss_function(U, V, X, test_indices):
     X_hat = np.matmul(U, V)
     X_hat[test_indices] = 0
-    
-    return get_static_value(0.5 * (norm(X - X_hat, 'fro', axis=[-2,-1]) ** 2))
+
+    return get_static_value(0.5 * (norm(X - X_hat, "fro", axis=[-2, -1]) ** 2))
+
 
 def gradient(U, A, V, B, X, T, D):
-    grad_U = ( np.matmul(np.matmul(U, A), np.transpose(A)) - np.matmul(T, np.transpose(A)) ) +\
-             ( np.matmul(np.matmul(U, V), np.transpose(V)) - np.matmul(X, np.transpose(V)) )
-             
-    grad_A =  np.matmul(np.matmul(np.transpose(U), U), A) - np.matmul(np.transpose(U), T)
-    
-    grad_V = ( np.matmul(np.matmul(np.transpose(U), U), V) - np.matmul(np.transpose(U), X) ) +\
-             ( np.matmul(np.matmul(np.transpose(B), B), V) - np.matmul(np.transpose(B), D) )
+    grad_U = (np.matmul(np.matmul(U, A), np.transpose(A)) - np.matmul(T, np.transpose(A))) + (
+        np.matmul(np.matmul(U, V), np.transpose(V)) - np.matmul(X, np.transpose(V))
+    )
+
+    grad_A = np.matmul(np.matmul(np.transpose(U), U), A) - np.matmul(np.transpose(U), T)
+
+    grad_V = (np.matmul(np.matmul(np.transpose(U), U), V) - np.matmul(np.transpose(U), X)) + (
+        np.matmul(np.matmul(np.transpose(B), B), V) - np.matmul(np.transpose(B), D)
+    )
 
     grad_B = np.matmul(np.matmul(B, V), np.transpose(V)) - np.matmul(D, np.transpose(V))
 
-    return [grad_U, grad_A, grad_V, grad_B]   
+    return [grad_U, grad_A, grad_V, grad_B]
